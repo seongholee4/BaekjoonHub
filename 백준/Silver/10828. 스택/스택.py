25ef3b9f8631ed4096b9cc6bb4512412dcdd2934
Seongho Lee <seongholee4@gmail.com>
2023-04-27 03:43:59 -0400

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -3,30 +3,36 @@ sys.setrecursionlimit(10**9)
 
 N = int(sys.stdin.readline().strip())
 
+def push(x):
+    stack.append(x)
+
+def pop() -> int:
+    if stack:
+        return stack.pop()
+    else:
+        return -1
+
+def size() -> int:
+    return len(stack)
+
+def empty() -> int:
+    return 0 if stack else 1
+
+def top() -> int:
+    return stack[len(stack)-1] if not empty() else -1 
+
 stack = []
 for _ in range(N):
     line = sys.stdin.readline().split()
-
-    if len(line) == 2: # push
-        command = line[0]
-        num = int(line[1])
-        stack.append(num)
-    else:
-        command = line[0]
-        if command == "pop":
-            if stack:
-                print(stack.pop())
-            else:
-                print(-1)
-        if command == "size":
-            print(len(stack))
-        if command == "empty":
-            if stack:
-                print(0)
-            else:
-                print(1)
-        if command == "top":
-            if stack:
-                print(stack[len(stack)-1])
-            else:
-                print(-1)
+    command = line[0]
+    if command == "push":
+        push(line[1])
+    if command == "pop":
+        print(pop())
+    if command == "size":
+        print(size())
+    if command == "empty":
+        print(empty())
+    if command == "top":
+        print(top())
+        
