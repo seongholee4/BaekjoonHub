448ab5172b494cfee654b187d537231016cd4e41
Seongho Lee <seongholee4@gmail.com>
2023-10-14 22:05:23 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 252 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,31 +1,12 @@
 import sys
+input = sys.stdin.readline
+ 
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+S = [0]*(N+1)
+for i in range(1, N+1):
+    S[i] = S[i-1] + arr[i-1]
 
-N, M = map(int, sys.stdin.readline().split())
-
-arr = list(map(int, sys.stdin.readline().split()))
-
-S = [0 for _ in range(N+1)]
-'''
-S[0] -> 0
-S[1] -> arr[0]
-S[2] -> arr[0] + arr[1]
-S[3] -> arr[0] + arr[1] + arr[2]
-...
-S[N] -> arr[0] + arr[1] + ... + arr[N-1]
-'''
-for i in range(N+1):
-    if i == 0:
-        S[i] = 0
-    else:
-        S[i] = S[i-1] + arr[i-1]
-        '''
-        S[1] = S[0] + arr[0] -> S[1] = 0 + arr[0] -> S[1] = arr[0]
-        S[2] = S[1] + arr[1] -> S[2] = arr[0] + arr[1]
-        S[3] = S[2] + arr[2] -> s[3] = arr[0] + arr[1] + arr[2]
-        ...
-        S[N] = S[N-1] + arr[N-1] -> S[N] = arr[0] + arr[1] + ... + arr[N-1]
-        '''
-
-for _ in range(M):
-    i, j = map(int, sys.stdin.readline().split())
+for q in range(M):
+    i, j = map(int, input().split())
     print(S[j] - S[i-1])
