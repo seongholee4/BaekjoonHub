94cf7dfc5d6656fc88c0a89057c1d20282f998a7
Seongho Lee <seongholee4@gmail.com>
2023-10-14 22:06:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 244 ms, Memory: 44272 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,12 +1,26 @@
 import sys
 input = sys.stdin.readline
- 
-N, M = map(int, input().split())
-arr = list(map(int, input().split()))
-S = [0]*(N+1)
-for i in range(1, N+1):
-    S[i] = S[i-1] + arr[i-1]
 
-for q in range(M):
-    i, j = map(int, input().split())
-    print(S[j] - S[i-1])
+class P11659_구간합구하기():
+    def __init__(self, N: int) -> None:
+        self.P_SUM = [0*(N+1)]
+
+    def 구간합(self, N: int, arr: list) -> None:
+        self.P_SUM[0] = arr[0]
+        for i in range(1, N):
+            self.P_SUM.append(arr[i]+self.P_SUM[i-1])
+            
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    s = P11659_구간합구하기(N)
+    s.구간합(N, arr)
+    for _ in range(M):
+        i, j = map(int, input().split())
+        if i == 1:
+            print(s.P_SUM[j-1])
+        else:
+            print(s.P_SUM[j-1] - s.P_SUM[i-2])
+        
+
